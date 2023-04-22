587904b74c6a0c95f2d6221e03eb0b7be803e426
ntk221 <nittakdub@gmail.com>
2023-04-22 17:15:49 +0900

    p.131まで

--- main.go
+++ main.go
@@ -5,20 +5,39 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func run(ctx context.Context) error {
 	s := &http.Server{
 		Addr: ":18080",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintln(w, "Hello, %s!", r.URL.Path[1:])
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
 	}
-	s.ListenAndServe()
-	// TODO: context.Contextを通じて処理のキャンセルを受け取った時，
-	// *http.Server.Shutdown()を呼び出してサーバーを終了させる
 
-	// TODO: run 関数の戻り値をerror型に変更し，エラーを返すようにする
+	// errgroup.WithContext()を使ってerrgroup.Groupを作成する
+	eg, ctx := errgroup.WithContext(ctx)
+
+	// TODO: errgroup.Go()を使って，別goroutineで，http.Server.ListenAndServe()を実行する
+	eg.Go(func() error {
+		if err := s.ListenAndServe(); err != nil &&
+			err != http.ErrServerClosed {
+			log.Printf("failed to close server: %v", err)
+			return err
+		}
+		return nil
+	})
+
+	// チャネルを使って，ctx.Done()が返す値を待つ
+	<-ctx.Done()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Printf("failed to shutdown server: %v", err)
+	}
+
+	// errgroup.Group.Wait()を使って，errgroup.Groupが完了するのを待つ
+	return eg.Wait()
 }
 
 func main() {
