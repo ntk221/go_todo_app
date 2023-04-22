7992d2fcca97ba69ead561e725bd5fb768821a42
ntk221 <nittakdub@gmail.com>
2023-04-22 18:01:02 +0900

    run 関数で，configの読み込みを行うように修正。前の実装に依存した，書き方になっているので，改めて，15章を読む

--- main.go
+++ main.go
@@ -4,12 +4,26 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
 
+	"github.com/ntk221/go_todo_app/config"
 	"golang.org/x/sync/errgroup"
 )
 
 func run(ctx context.Context) error {
+	cfg, err := config.New()
+	if err != nil {
+		return err
+	}
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
+	}
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("start with: %v", url)
+
 	s := &http.Server{
 		Addr: ":18080",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -43,5 +57,6 @@ func run(ctx context.Context) error {
 func main() {
 	if err := run(context.Background()); err != nil {
 		log.Printf("failet to terminate server: %v", err)
+		os.Exit(1)
 	}
 }
